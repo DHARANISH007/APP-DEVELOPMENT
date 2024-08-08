99558515346fa811ad1a2a174cd552a8d54d44b3
DHARANISH007 <124783248+DHARANISH007@users.noreply.github.com>
2024-08-08 15:29:47 +0530

    Add files via upload

---
+++ Toystore/toystore/src/main/java/com/toystore/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.toystore.config;
+
+public class JwtConstant {
+   public static String SECRET_KEY="budlbnsekjfpoaejfvshjkbdf sdkufhnsufgbjksdfhikln";
+   public static String JWT_HEADER="Authorization";
+}
